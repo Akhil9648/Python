c727f15c37a669d931a353e741a98bac5e96dba9
Akhil Pandey <akhilpandey494@gmail.com>
2025-03-26 16:06:36 +0530

    Take the name of seven fruits as input and enter in list

---
+++ Add fruits name in list.py
@@ -0,0 +1,7 @@
+# Take the name of seven fruits as input and enter in list
+n=int(input("Enter the number of fruits"))
+fruits=[]
+for i in range (n):
+    name=input("Enter fruit name:")
+    fruits.append(name)
+print(fruits)
