c42dfd4d729212e6040a195bf83b221aec04129a
Akhil Pandey <akhilpandey494@gmail.com>
2025-03-26 12:17:07 +0530

    Reverse a string except special symbols

---
+++ Reverse string.py
@@ -0,0 +1,15 @@
+ipt = "in@#fytq&!"
+lst = list(ipt)
+i, j  = 0, len(ipt) - 1
+
+while i < len(ipt)//2:
+    if lst[i].isalnum() and lst[j].isalnum():
+        lst[i], lst[j] = lst[j], lst[i]
+        print(lst)
+        i, j = i+1, j-1
+        
+    elif not lst[i].isalnum():
+        i += 1
+    else:
+        j -= 1
+print("".join(lst))
