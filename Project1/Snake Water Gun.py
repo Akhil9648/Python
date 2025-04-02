d024badd6d5d816f30d0d3f43392edb16df2ba23
Akhil Pandey <akhilpandey494@gmail.com>
2025-04-02 12:21:02 +0530

    Snake Water Gun.py

--- Project1/Snake Water Gun.py
+++ Project1/Snake Water Gun.py
@@ -1,6 +1,9 @@
 import random
 comp=random.choice([-1,0,1])
 you=int(input("Enter a number between 1 for Snake,0 For Water,-1 for Gun:"))
+if(you>1 or you<-1):
+    print("Something Went Wrong")
+    exit(0)
 youdict={1:"Snake",0:"Water",-1:"Gun"}
 print(f"You chose:{youdict[you]} and Computer chose:{youdict[comp]}")
 if comp==you:
