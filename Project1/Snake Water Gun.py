53f58c3f5a7ba406fa1277a7706bcf3e2b8550ad
Akhil Pandey <akhilpandey494@gmail.com>
2025-04-02 12:12:17 +0530

    Create Snake Water Gun.py

---
+++ Project1/Snake Water Gun.py
@@ -0,0 +1,22 @@
+import random
+comp=random.choice([-1,0,1])
+you=int(input("Enter a number between 1 for Snake,0 For Water,-1 for Gun:"))
+youdict={1:"Snake",0:"Water",-1:"Gun"}
+print(f"You chose:{youdict[you]} and Computer chose:{youdict[comp]}")
+if comp==you:
+    print("Its a Draw")
+else:
+    if (comp==1 and you==-1):
+        print("You Win")
+    elif (comp==1 and you==0):
+        print("You Lose")
+    elif (comp==0 and you==-1):
+        print("You Lose")
+    elif (comp==0 and you==1):
+        print("You Win")
+    elif (comp==-1 and you==0):
+        print("You Lose")
+    elif (comp==-1 and you==1):
+        print("You Win")
+    else:
+        print("Something Went Wrong")
