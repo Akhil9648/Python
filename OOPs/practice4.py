ddd22a1e9a7df8aa42641f5d70457f2251a3a4ae
Akhil Pandey <akhilpandey494@gmail.com>
2025-04-04 14:41:13 +0530

    Create practice4.py

---
+++ OOPs/practice4.py
@@ -0,0 +1,11 @@
+class calculator:
+    def __init__(self,square,cube,root):
+        print("Hello")
+        self.square=square
+        self.cube=cube
+        self.root=root
+    def calculate(self):
+        print(f"The squere of the number is {self.square},Cube of the number is {self.cube} and the root of the number is {self.root}")
+n=int(input("Enter a number"))
+a=calculator(n*n,n*n*n,n*(0.5))
+a.calculate()
