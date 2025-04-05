df2a13fc89608cbd793f956ed36582efd6394224
Akhil Pandey <akhilpandey494@gmail.com>
2025-04-06 03:10:53 +0530

    Create Match_case.py

---
+++ Advanced_PythonCWH/Match_case.py
@@ -0,0 +1,14 @@
+def http_status(status):
+    match status:
+        case 200:
+            return "OK"
+        case 404:
+            return "Not found"
+        case 500:
+            return "Internal Server Error"
+        case _:
+            return "Unknown Status"
+print(http_status(200))
+print(http_status(404))
+print(http_status(500))
+print(http_status(2007))
