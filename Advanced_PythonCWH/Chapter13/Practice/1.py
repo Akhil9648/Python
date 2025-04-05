3940b4a7f052e0d15a26efddc0daab9551c038c2
Akhil Pandey <akhilpandey494@gmail.com>
2025-04-06 04:26:11 +0530

    Create 1.py

---
+++ Advanced_PythonCWH/Chapter13/Practice/1.py
@@ -0,0 +1,5 @@
+a=input("Enter the name")
+b=int(input("Enter the marks"))
+c=input("Enter the Mobile Number")
+s="The name of student is {} his marks is {}% and phone number is {}".format(a,b,c)
+print(s)
