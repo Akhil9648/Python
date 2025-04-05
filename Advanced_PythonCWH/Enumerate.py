2e4f6bb093e1f2155030109b1197ac674ccc452d
Akhil Pandey <akhilpandey494@gmail.com>
2025-04-06 03:12:35 +0530

    Create Enumerate.py

---
+++ Advanced_PythonCWH/Enumerate.py
@@ -0,0 +1,8 @@
+l=[2,18,22,23,24,32,39]
+index=0
+# for i in l:
+#     print(f"The item at index {index} is {i}")
+#     index+=1
+# This can be simplified using Enumerate function
+for index,i in enumerate(l):
+    print(f"The item at index {index} is {i}")
